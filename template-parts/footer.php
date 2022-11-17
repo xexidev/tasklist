a60e4b53cbb834f326124e8eefde5713977d5477
setade <dasack@gmail.com>
2022-11-17 13:14:17 +0100

    Linked to GitHub

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="footer-text">
                 <a href="./doces.php" title="Documentación (ES)" target="_blank">Documentación (ES)</a> · 
-                <a href="./files/xexitasklist.rar" title="Download project" target="_blank">Download project</a> · 
+                <a href="https://github.com/xexidev/tasklist" title="View project on GitHub" target="_blank">GitHub</a> · 
                 <a href="https://www.xexi.es" title="xexi.es" target="_blank">xexi.es</a> · 
                 <a href="mailto:hola@xexi.es" title="hola@xexi.es" target="_blank">hola@xexi.es</a> · 
                 xexi 2022
